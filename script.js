38085165ca7cdc425e9f200f355cb1fcf2b896d0
Andrew Moshkovich <thepythonhelper@gmail.com>
2023-05-01 18:06:04 -0700

    completed activity

--- script.js
+++ script.js
@@ -20,9 +20,12 @@ function success(position){
 	// Print out the latitude and longitude to see if it works!
 
 }
-    
-    
-/*btn.addEventListener("click", function(){
-        //code here
+const btn = document.getElementById('getWeatherBtn');
+btn.addEventListener("click", function(){
+  var forecast = [["M", 52], ["Tu", 53], ["W", 54], ["Th", 55], ["F", 56]] // Nested array of predefined day/forecast pairs
+  var forecastElements = document.getElementsByClassName("forecast"); // Setting forecastElements to an array of divs with the class 'forecast' (5 in this case): [first div, second div, third div]
+  for (var i = 0; i < forecast.length; i++) { // For loop that goes from the 0th index to the length - 1 index
+      forecastElements[i].innerHTML = forecast[i][0] + ": " + forecast[i][1] + "\u00B0F"; // Sets the innerHTML of the i'th 'forecast' div element to a string in the format of "Day: Temp°F"
+    }
 })
-*/
+
